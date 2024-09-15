3b1be9688b18165fbb64e1853c126599a1af7a05
thor <thor@colourbox.com>
2024-09-15 16:34:14 +0200

    Messing around with routes.

--- angular_shop/src/app/app.routes.ts
+++ angular_shop/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
-        '/home': 'home',
-        '/cart': 'cart',
-        '/profile': 'profile',
+        path: 'profile',
+        component: ProfileComponent,
+    },
+    {
+        path: 'cart',
+        component: CartComponent,
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
     }
 ];
--- angular_shop/src/app/header/header.component.ts
+++ angular_shop/src/app/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink, RouterLinkActive, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIcon, MatToolbarModule, MatButtonModule],
+  imports: [MatIcon, MatToolbarModule, MatButtonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public currentRoute: string = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+      }
+    });
+  }
 }
